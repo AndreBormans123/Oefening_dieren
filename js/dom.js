61f4b477bf939b1e965f818b3ceff34a519c05d8
Andre Bormans <andre.bormans@student.ucll.be>
2022-12-29 18:35:38 +0100

    Added dom.js code

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,21 @@
+const clearStatus = (status) =>
+  (document.getElementById("status").innerHTML = document.getElementById(
+    "status"
+  ).innerHTML =
+    "");
+
 const addStatus = (status) =>
   (document.getElementById("status").innerHTML =
     document.getElementById("status").innerHTML + "<p>" + status + "</p>");
+
+const printAnimals = (animals, status) => {
+  if (status) {
+    addStatus(status);
+  } else {
+    clearStatus();
+  }
+  for (const animal of animals) {
+    printAnimals(animal);
+  }
+  addStatus("-----");
+};
